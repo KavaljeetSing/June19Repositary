9e525e4445f0a84a67c5b69d891464c2584bab17
Kavaljeet Singh <748291@student.tor.loc>
2019-06-19 14:57:00 -0400

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Helo Class!!");
+//Factory Function
+function createCircle(radius){
+return{
+     radius,
+    draw: function(){
+        console.log('draw');
+    }
+};
+}
+const circle = createCircle(1);
+circle.draw();
+
+//Constructor function
+function Circle(radius){
+    console.log('this',this);
+    this.radius=radius;
+    this.draw = function(){
+        console.log('draw');
+    }
+}
+
+const another = new Circle(1);
+
